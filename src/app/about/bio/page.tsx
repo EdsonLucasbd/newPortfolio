697e1c5846814a729971b3d48c964299335159f9
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2023-12-18 14:31:12 -0300

    refactor: update bio

--- src/app/about/bio/page.tsx
+++ src/app/about/bio/page.tsx
@@ -1,12 +1,19 @@
 import { Content } from '@/components/Content'
+import Image from 'next/image'
+import MyPhoto from '../../../../public/portfolio_photo.png'
 
 export const metadata = {
   title: 'Sobre',
 }
 
 export default async function Bio() {
-  return <Content
-    queryUrl='https://gist.githubusercontent.com/EdsonLucasbd/188206371a9fdf5c27d37b37d62c3995/raw/b9038f91de73db01aea0633bb6e8f300ec222895/gistfile1.txt'
-    lang='javascript'
-  />
+  return (
+    <div className="flex flex-row justify-between">
+      <Content
+        queryUrl='https://gist.githubusercontent.com/EdsonLucasbd/188206371a9fdf5c27d37b37d62c3995/raw/6f83dd8b258b43e701c572c346024434d264ac76/gistfile1.txt'
+        lang='javascript'
+      />
+      <Image src={MyPhoto} alt='' aria-hidden className='w-2/5 h-auto mr-12' />
+    </div>
+  )
 }
