09b8c8ffdae7f5f93bb7fcfcb12d7ee338a18b40
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-01-03 18:16:00 -0300

    feat: :sparkles: mobile menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,19 @@ export default async function Hello() {
   const code = theme.codeToHtml('const github = "https://github.com/EdsonLucasbd/newPortfolio"', { lang: 'javascript' })
 
   return (
-    <div className='w-full h-full flex flex-row items-center justify-evenly
+    <div className='w-min md:w-full h-full flex flex-row items-center justify-evenly
       py-[180px]'>
       <div className="flex flex-col animate-fade-in-bottom-soft">
         <div className="flex flex-col text-secondary-200 gap-2">
-          <p className='text-lg'>Olá, me chamo</p>
-          <h1 className='text-6xl -indent-1'>Edson Lucas</h1>
-          <p className='text-3xl text-secondary-500 flex flex-row gap-1'>
+          <p className='text-base md:text-lg'>Olá, me chamo</p>
+          <h1 className='text-3xl md:text-6xl -indent-1'>Edson Lucas</h1>
+          <p className='text-xl md:text-3xl text-secondary-500 flex flex-row gap-1'>
             <span className='animate-pulse'>{'>'}</span>
             Front-end developer
           </p>
         </div>
 
-        <div className="flex flex-col mt-20 gap-2">
+        <div className="hidden md:flex flex-col mt-20 gap-2">
           <p className='text-secondary-300'>{'// complete o jogo para continuar'}</p>
           <p className='text-secondary-300'>{'// você também pode conferir isso no meu github'}</p>
           <a
@@ -39,7 +39,7 @@ export default async function Hello() {
         <span className='absolute bottom-0 right-0 flex blur-3xl bg-secondary-500 w-[400px] 
             h-[200px] -rotate-12 rounded-full'
         />
-        <iframe className='hidden lg:block relative w-[28.125rem] h-[420px] rounded-lg' src='https://js-snake-sigma.vercel.app/' title='javascript snake game' height={475} />
+        <iframe className='hidden md:block relative w-[28.125rem] h-[420px] rounded-lg' src='https://js-snake-sigma.vercel.app/' title='javascript snake game' height={475} />
       </div>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
+import { Menu, X } from 'lucide-react';
 
 export const Header = () => {
   const path = usePathname()
   const escapedUrl = path.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
   const regex = new RegExp(`^${escapedUrl}$`)
+  const [isMobile, setIsMobile] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   const options = [
     {
@@ -26,11 +29,21 @@ export const Header = () => {
     },
   ]
 
+  useEffect(() => {
+    const screenWidth = window.innerWidth
+
+    if (screenWidth < 768) {
+      setIsMobile(true)
+    } else setIsMobile(false)
+
+  }, [isMobile])
+
   return (
     <div className='w-full h-14 flex flex-row
-    text-secondary-300 border-b border-primary-100 bg-primary-300'>
+    text-secondary-300 border-b border-primary-100 bg-primary-300
+     justify-between md:justify-normal'>
       <span className='py-[17px] pl-[22px] pr-24'>edson-lucas</span>
-      {options.map((option) => (
+      {!isMobile && options.map((option) => (
         <Link
           key={option.title}
           href={option.link}
@@ -42,11 +55,51 @@ export const Header = () => {
           {option.title}
         </Link>
       ))}
-      <Link href='/' className='py-[17px] px-[31px] border-l border-primary-100 ml-auto
-      hover:text-secondary-200 transition-colors ease-in-out duration-200'
-      >
-        _contact-me
-      </Link>
+      {!isMobile ? (
+
+        <Link href='/' className='py-[17px] px-[31px] border-l border-primary-100 ml-auto
+        hover:text-secondary-200 transition-colors ease-in-out duration-200'
+        >
+          _contact-me
+        </Link>
+
+      ) : (
+        <div className='flex items-center justify-center pr-[22px]'>
+          <Menu
+            role='button'
+            className='cursor-pointer'
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          <div data-open={isOpen} className="hidden data-[open=true]:flex 
+            flex-col items-center
+            absolute top-[48.2%] left-1/2 -translate-x-1/2 
+            -translate-y-1/2 w-[91%] h-[92%] bg-primary-200 rounded-lg 
+            ring-1 ring-primary-100 z-20">
+            <div className="flex flex-row justify-between w-full">
+              <span className='my-[17px] ml-[22px]'>edson-lucas</span>
+              <X
+                role='button'
+                className='my-[17px] mr-[18px]'
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            </div>
+            <div className='flex flex-col w-full'>
+              {isOpen && options.map((option) => (
+                <Link
+                  key={option.title}
+                  href={option.link}
+                  className='first:border-b-0 last:border-t-0 border-y py-[17px] px-[31px] even:border-l even:border-r border-primary-100
+                  hover:text-secondary-200 transition-colors ease-in-out duration-200'
+                >
+                  {option.title}
+                </Link>
+              ))}
+
+            </div>
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }
