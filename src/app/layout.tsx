e5d85e03116e9e1c57e37e1a2f4f862d3b7ea0b2
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-03-14 20:32:29 -0300

    update ads srcipt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ const pressStart2P = Press_Start_2P({
 export const metadata: Metadata = {
   title: 'Edson Lucas',
   description: 'Portfólio',
+  other: {
+    name: "google-adsense-account",
+    content: "ca-pub-7594484605572689"
+  }
 }
 
 
@@ -32,13 +36,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7594484605572689"
-          crossOrigin="anonymous">
-        </script>
-      </Head>
       <body className={`${fira.className} ${pressStart2P.variable} m-[15px] md:m-[45px] p-0 bg-black `}>
         <main className='bg-primary-200 rounded-lg ring-1 ring-primary-100'>
           <ApolloWrapper>
